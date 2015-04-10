6754e61e923fec0caa66d2604a5c41cc7aa4d78e
gogojojo <joseph.gochua@gmail.com>
2015-04-09 21:00:49 -0700

    added main paragraph

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ $(function(){
     $('object').attr('class','');
     $('.overlay').css("display","none")
   })
-})
+  $('#button').on('click', function(e){
+      e.preventDefault();
+      var target= $('#main');
+      $('html, body').stop().animate({
+         scrollTop: target.offset().top
+      }, 2000);
+  });
+});
